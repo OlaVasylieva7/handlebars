4c227d1a321497cde1b300888e6d1b5ebb418755
OlaVasylieva7 <olhavas74@gmail.com>
2024-07-31 22:39:31 +0300

    update

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -154,6 +154,66 @@ var minions = exports.minions = [{
   appearance: "medium height, one eye",
   character: "a prankster and loves pranks",
   association: "megaphone"
+}, {
+  name: "Kevin",
+  img: "https://img.pixers.pics/pho(s3:700/PI/62/8/700_PI628_0a52c36a534004114ce074067697190e_5b7abc84a0477_.,700,653,jpg)/plakaty-minion-kevin.jpg.jpg",
+  appearance: "tall, two eyes",
+  character: "brave and cares about other Minions",
+  association: "nothing"
+}, {
+  name: "Stuart",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzzdBKT9ZG3XzZ5EkqgQujgYhXq6H_0IdnoQ&s",
+  appearance: "medium height, one eye",
+  character: "lazy, but charming and likable",
+  association: "guitar"
+}, {
+  name: "Bob",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhVbYP2PSs2qGf2eymjf1ZzKDkyrhbaXIcweXL7ZiJFdprBjRt4vg5l1bys7jedlRcH3U&usqp=CAU",
+  appearance: "short, two different colored eyes",
+  character: "innocent, childlike and loves teddy bears",
+  association: "teddy bear"
+}, {
+  name: "Dave",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfS5B3KlUIdxmSNeseCRfsOuTK3x7NeQevEije1W0Wvzeh6qLHiuARQrI9E5YmOOKxQ7o&usqp=CAU",
+  appearance: "medium height, two eyes",
+  character: "kind, funny and loves video games",
+  association: "phone"
+}, {
+  name: "Carl",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRisa8lJBNgV_d_q4GC7Es2kimzSE6hf-s0Rdj3tc0jkuvhUafjXdiBDWHxZjugu_xYHLI&usqp=CAU",
+  appearance: "medium height, one eye",
+  character: "a prankster and loves pranks",
+  association: "megaphone"
+}, {
+  name: "Kevin",
+  img: "https://img.pixers.pics/pho(s3:700/PI/62/8/700_PI628_0a52c36a534004114ce074067697190e_5b7abc84a0477_.,700,653,jpg)/plakaty-minion-kevin.jpg.jpg",
+  appearance: "tall, two eyes",
+  character: "brave and cares about other Minions",
+  association: "nothing"
+}, {
+  name: "Stuart",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzzdBKT9ZG3XzZ5EkqgQujgYhXq6H_0IdnoQ&s",
+  appearance: "medium height, one eye",
+  character: "lazy, but charming and likable",
+  association: "guitar"
+}, {
+  name: "Bob",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhVbYP2PSs2qGf2eymjf1ZzKDkyrhbaXIcweXL7ZiJFdprBjRt4vg5l1bys7jedlRcH3U&usqp=CAU",
+  appearance: "short, two different colored eyes",
+  character: "innocent, childlike and loves teddy bears",
+  association: "teddy bear"
+}, {
+  name: "Dave",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfS5B3KlUIdxmSNeseCRfsOuTK3x7NeQevEije1W0Wvzeh6qLHiuARQrI9E5YmOOKxQ7o&usqp=CAU",
+  appearance: "medium height, two eyes",
+  character: "kind, funny and loves video games",
+  association: "phone"
+}, {
+  name: "Carl",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRisa8lJBNgV_d_q4GC7Es2kimzSE6hf-s0Rdj3tc0jkuvhUafjXdiBDWHxZjugu_xYHLI&usqp=CAU",
+  appearance: "medium height, one eye",
+  character: "a prankster and loves pranks",
+  association: "megaphone"
 }];
 },{}],"../node_modules/handlebars/dist/handlebars.runtime.js":[function(require,module,exports) {
 var define;
@@ -2641,7 +2701,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52198" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52313" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
